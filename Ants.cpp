abdd5d40f1cd86b0de28b911d1a786c6dd14f36d
Abkaal <kacper@kurzeja.net>
2015-12-13 19:02:34 +0100

    Pierwsze pliki

---
+++ Ants.cpp
@@ -0,0 +1,9 @@
+#include "Ants.hpp"
+
+void AntIt(int** san, int MAX){
+    bool* han; han = new bool[MAX];
+    for(int I=0; I<MAX, ++I) han[I]=0;
+
+
+
+}
---
+++ Ants.hpp
@@ -0,0 +1,6 @@
+#ifndef Ants_hpp
+#define Ants_hpp
+
+void AntIt(int** graph, int MAX);
+
+#endif // Ants_hpp
